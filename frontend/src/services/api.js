9871a5ef90cfd1e50ada7904165bc08af750ec63
Andria SIlva <andria2034@gmail.com>
2026-08-06 22:45:16 +0100

    UpdateAPIConfig

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const API_BASE = '/api'
+const API_ORIGIN = import.meta.env.VITE_API_URL || ''
+const API_BASE = `${API_ORIGIN.replace(/\/$/, '')}/api`
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token')
